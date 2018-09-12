2c72cb5511028492aaec4b5be18ce7ab833a24e2
Minh <ntminhdn@gmail.com>
2018-09-12 11:55:06 +0700

    fix 1 dòng

--- app/src/main/java/com/example/huy/design/KQKhongLienHop.java
+++ app/src/main/java/com/example/huy/design/KQKhongLienHop.java
@@ -6,10 +6,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class KQKhongLienHop extends AppCompatActivity {
-    TextView txtAnc,txtSnct, txtYncd,txtYnct,txtInc,txtSncd,txtBsDT, txtBsDN, txtAltDT, txtAltDN, txtSlttDT;
-    TextView txtSlttDN,txtYltdDT, txtYltdDN, txtYlttDT, txtYlttDN, txtIltDT, txtIltDN ;
-
-
+    TextView txtAnc, txtSnct, txtYncd, txtYnct, txtInc, txtSncd, txtBsDT, txtBsDN, txtAltDT, txtAltDN, txtSlttDT;
+    TextView txtSlttDN, txtYltdDT, txtYltdDN, txtYlttDT, txtYlttDN, txtIltDT, txtIltDN;
 
 
     @Override
@@ -21,62 +19,53 @@ public class KQKhongLienHop extends AppCompatActivity {
 
 
     }
-    public void addEvents(){
-        //Láy kết quả
-        Intent KLHintent= getIntent();
-        Bundle KLHbundle = KLHintent.getBundleExtra("KLHbundle");
-        txtAnc.setText(""+ KLHbundle.getDouble("Anc"));
-        txtSnct.setText(""+ KLHbundle.getDouble("Snct"));
-        txtYncd.setText(""+ KLHbundle.getDouble("Yncd"));
-        txtYnct.setText(""+ KLHbundle.getDouble("Ynct"));
-        txtInc.setText(""+ KLHbundle.getDouble("Inc"));
-        txtSncd.setText(""+ KLHbundle.getDouble("Sncd"));
-        txtBsDT.setText(""+ KLHbundle.getDouble("BsDT"));
-        txtBsDN.setText(""+ KLHbundle.getDouble(" BsDN"));
-        txtAltDT.setText(""+ KLHbundle.getDouble("AltDT"));
-        txtAltDN.setText(""+ KLHbundle.getDouble(" AltDN"));
-        txtSlttDT.setText(""+ KLHbundle.getDouble(" SlttDT"));
-        txtSlttDN.setText(""+ KLHbundle.getDouble(" SlttDN"));
-        txtYltdDT.setText(""+ KLHbundle.getDouble("YltdDT"));
-        txtYltdDN.setText(""+ KLHbundle.getDouble("YltdDN"));
-        txtYlttDT.setText(""+ KLHbundle.getDouble("YlttDT"));
-        txtYlttDN.setText(""+ KLHbundle.getDouble("YlttDN"));
-        txtIltDT.setText(""+ KLHbundle.getDouble("IltDT"));
-        txtIltDN.setText(""+ KLHbundle.getDouble("IltDN"));
 
+    public void addEvents() {
+        //Láy kết quả
+        Intent KLHintent = getIntent();
+        Bundle KLHbundle = KLHintent.getBundleExtra("KLHbundle");
+        txtAnc.setText(String.valueOf(KLHbundle.getDouble("Anc")).length() > 6 ? String.valueOf(KLHbundle.getDouble("Anc")).substring(0, 6) : String.valueOf(KLHbundle.getDouble("Anc")));
+        txtSnct.setText("" + KLHbundle.getDouble("Snct"));
+        txtYncd.setText("" + KLHbundle.getDouble("Yncd"));
+        txtYnct.setText("" + KLHbundle.getDouble("Ynct"));
+        txtInc.setText("" + KLHbundle.getDouble("Inc"));
+        txtSncd.setText("" + KLHbundle.getDouble("Sncd"));
+        txtBsDT.setText("" + KLHbundle.getDouble("BsDT"));
+        txtBsDN.setText("" + KLHbundle.getDouble(" BsDN"));
+        txtAltDT.setText("" + KLHbundle.getDouble("AltDT"));
+        txtAltDN.setText("" + KLHbundle.getDouble(" AltDN"));
+        txtSlttDT.setText("" + KLHbundle.getDouble(" SlttDT"));
+        txtSlttDN.setText("" + KLHbundle.getDouble(" SlttDN"));
+        txtYltdDT.setText("" + KLHbundle.getDouble("YltdDT"));
+        txtYltdDN.setText("" + KLHbundle.getDouble("YltdDN"));
+        txtYlttDT.setText("" + KLHbundle.getDouble("YlttDT"));
+        txtYlttDN.setText("" + KLHbundle.getDouble("YlttDN"));
+        txtIltDT.setText("" + KLHbundle.getDouble("IltDT"));
+        txtIltDN.setText("" + KLHbundle.getDouble("IltDN"));
 
 
     }
-    public void  addConrols(){
-        txtAnc=(TextView) findViewById(R.id.txtAnc);
-        txtSnct=(TextView) findViewById(R.id.txtSnct);
-        txtYncd=(TextView) findViewById(R.id.txtYncd);
-        txtYnct=(TextView) findViewById(R.id.txtYnct);
-        txtInc=(TextView) findViewById(R.id.txtInc);
-        txtSncd=(TextView) findViewById(R.id.txtSncd);
+
+    public void addConrols() {
+        txtAnc = (TextView) findViewById(R.id.txtAnc);
+        txtSnct = (TextView) findViewById(R.id.txtSnct);
+        txtYncd = (TextView) findViewById(R.id.txtYncd);
+        txtYnct = (TextView) findViewById(R.id.txtYnct);
+        txtInc = (TextView) findViewById(R.id.txtInc);
+        txtSncd = (TextView) findViewById(R.id.txtSncd);
         //liên hợp dài hạn
-        txtBsDT=(TextView) findViewById(R.id.txtBsDT);
-        txtBsDN=(TextView) findViewById(R.id.txtBsDN);
-        txtAltDT=(TextView) findViewById(R.id.txtAltDT);
-        txtAltDN=(TextView) findViewById(R.id.txtAltDN);
-        txtSlttDT=(TextView) findViewById(R.id.txtSlttDT);
-        txtSlttDN=(TextView) findViewById(R.id.txtSlttDN);
-        txtYltdDT=(TextView) findViewById(R.id.txtYltdDT);
-        txtYltdDN=(TextView) findViewById(R.id.txtYltdDN);
-        txtYlttDT=(TextView) findViewById(R.id.txtYlttDT);
-        txtYlttDN=(TextView) findViewById(R.id.txtYlttDN);
-        txtIltDT=(TextView) findViewById(R.id.txtIltDT);
-        txtIltDN=(TextView) findViewById(R.id.txtIltDN);
-
-
-
-
-
-
-
-
-
-
+        txtBsDT = (TextView) findViewById(R.id.txtBsDT);
+        txtBsDN = (TextView) findViewById(R.id.txtBsDN);
+        txtAltDT = (TextView) findViewById(R.id.txtAltDT);
+        txtAltDN = (TextView) findViewById(R.id.txtAltDN);
+        txtSlttDT = (TextView) findViewById(R.id.txtSlttDT);
+        txtSlttDN = (TextView) findViewById(R.id.txtSlttDN);
+        txtYltdDT = (TextView) findViewById(R.id.txtYltdDT);
+        txtYltdDN = (TextView) findViewById(R.id.txtYltdDN);
+        txtYlttDT = (TextView) findViewById(R.id.txtYlttDT);
+        txtYlttDN = (TextView) findViewById(R.id.txtYlttDN);
+        txtIltDT = (TextView) findViewById(R.id.txtIltDT);
+        txtIltDN = (TextView) findViewById(R.id.txtIltDN);
 
 
     }
