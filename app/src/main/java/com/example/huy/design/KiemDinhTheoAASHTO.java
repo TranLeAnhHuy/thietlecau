e180ff89df242349e48fb17b86075a54a2e3cb22
Minh <ntminhdn@gmail.com>
2018-11-25 10:15:41 +0700

    abc

--- app/src/main/java/com/example/huy/design/KiemDinhTheoAASHTO.java
+++ app/src/main/java/com/example/huy/design/KiemDinhTheoAASHTO.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.applandeo.FilePicker;
+import com.applandeo.constants.FileType;
+import com.applandeo.listeners.OnSelectFileListener;
 
-import java.util.regex.Pattern;
-
-//import com.nbsp.materialfilepicker.MaterialFilePicker;
-//import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+import java.io.File;
 
 public class KiemDinhTheoAASHTO extends AppCompatActivity {
     Button btnNhapFile;
+    File myFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,27 +22,27 @@ public class KiemDinhTheoAASHTO extends AppCompatActivity {
         addEvents();
         addControls();
     }
-    public void addEvents(){
-        btnNhapFile=(Button) findViewById(R.id.btnNhapFile);
+
+    public void addEvents() {
+        btnNhapFile = (Button) findViewById(R.id.btnNhapFile);
 
     }
-    public void addControls(){
+
+    public void addControls() {
         NhapFie();
 
     }
-    public void NhapFie(){
+
+    public void NhapFie() {
         btnNhapFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new MaterialFilePicker()
-                        ////dàed
-                        .withActivity(KiemDinhTheoAASHTO.this)
-                        .withRequestCode(1000)
-                        .withFilter(Pattern.compile(".*\\.txt$")) // Filtering files and directories by file name using regexp
-                        .withFilterDirectories(true) // Set directories filterable (false by default)
-                        .withHiddenFiles(true) // Show hidden files and folders
-                        .start();
-
+                new FilePicker.Builder(KiemDinhTheoAASHTO.this, new OnSelectFileListener() {
+                    @Override
+                    public void onSelect(File file) {
+                        myFile = file;
+                    }
+                }).fileType(FileType.TEXT).show();
             }
         });
     }
