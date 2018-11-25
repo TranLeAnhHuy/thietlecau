6e3b1b66e1b63e0616a497e5b9cec8c32c0f3f94
AnhHuy <tranhuy27396@gmail.com>
2018-11-25 09:58:32 +0700

    commit and push

--- app/src/main/java/com/example/huy/design/KiemDinhTheoAASHTO.java
+++ app/src/main/java/com/example/huy/design/KiemDinhTheoAASHTO.java
@@ -1,11 +1,16 @@
 package com.example.huy.design;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+
 import java.util.regex.Pattern;
+
+//import com.nbsp.materialfilepicker.MaterialFilePicker;
+//import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
 public class KiemDinhTheoAASHTO extends AppCompatActivity {
     Button btnNhapFile;
@@ -32,11 +37,12 @@ public class KiemDinhTheoAASHTO extends AppCompatActivity {
                 new MaterialFilePicker()
                         ////dàed
                         .withActivity(KiemDinhTheoAASHTO.this)
-                        .withRequestCode(1)
+                        .withRequestCode(1000)
                         .withFilter(Pattern.compile(".*\\.txt$")) // Filtering files and directories by file name using regexp
                         .withFilterDirectories(true) // Set directories filterable (false by default)
                         .withHiddenFiles(true) // Show hidden files and folders
                         .start();
+
             }
         });
     }
