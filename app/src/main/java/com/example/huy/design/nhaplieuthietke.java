e8d760613351e6b0631bb5a92a093f66784aefde
AnhHuy <tranhuy27396@gmail.com>
2019-01-14 11:53:58 +0700

    commit and push

--- app/src/main/java/com/example/huy/design/nhaplieuthietke.java
+++ app/src/main/java/com/example/huy/design/nhaplieuthietke.java
@@ -154,6 +154,7 @@ public class nhaplieuthietke extends AppCompatActivity {
     String tpn;
     AppDatabase appDatabase;
     String bt;
+    int o;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
